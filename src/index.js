85d7a3ee2f108fc010e5314e0a1f24da731e2ec2
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2025-03-19 17:03:08 +0100

    change the index

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { menu } from './menu';
 
 const restaurantPage = function() {
     const header = document.getElementById('content');
-     header.appendChild(Home());
-    header.appendChild(menu());
+     //header.appendChild(Home());
+    //header.appendChild(menu());
 
     const btns = document.querySelectorAll('#btn');
     btns.forEach(function (button) {
         button.addEventListener('click',function (e) {
-            header.innerHTML = ''
+           // header.innerHTML = ''
         if (e.target.textContent === 'home') {
             header.appendChild(Home()); 
         }else if(e.target.textContent === 'menu'){
-            header.appendChild(menu())
+            header.appendChild(Menu())
         }
             
         })
@@ -23,6 +23,9 @@ const restaurantPage = function() {
     })
     
 }
+
+
+
 
 restaurantPage();
 
