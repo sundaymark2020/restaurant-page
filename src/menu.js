3f77632a8c36e8317cf98c28d5bf543ffc8e90ff
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2025-03-20 15:05:04 +0100

    add the image

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,22 @@
+import vegetable from './images/vegetable-soup.jpg';
+
+
 const getMenu = function () {
   const menuDiv = document.createElement("div");
   const menuHeading = document.createElement("h1");
-  menuHeading.textContent = "Menu";
+  menuHeading.textContent = "MENU";
   menuHeading.classList.add("menuDiv");
   menuDiv.appendChild(menuHeading);
+
+  const vegetableSoup = document.createElement('h1')
+  vegetableSoup.textContent = 'Vegetable Soup';
+  menuDiv.appendChild(vegetableSoup);
+
+  const img1 = document.createElement('img');
+  img1.src = vegetable;
+  menuDiv.appendChild(img1);
+  img1.classList.add('img1');
+
   return menuDiv
 };
 
