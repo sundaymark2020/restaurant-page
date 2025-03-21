3624d3fd781db6b7b97b7d49086bbe5af760362a
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2025-03-21 14:59:21 +0100

    create the last image

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
 import vegetable from './images/vegetable-soup.jpg';
+import jellofrice from './images/jellof-rice.jpg';
+import bean from './images/beans.jpg';
 
 
 const getMenu = function () {
@@ -16,6 +18,27 @@ const getMenu = function () {
   img1.src = vegetable;
   menuDiv.appendChild(img1);
   img1.classList.add('img1');
+  //jellof rice section
+
+  const jellof = document.createElement('h1');
+  jellof.textContent = 'Jellof-Rice';
+  menuDiv.appendChild(jellof);
+
+  const img2 = document.createElement('img');
+  img2.src = jellofrice;
+  img2.classList.add('img2')
+  menuDiv.appendChild(img2);
+
+  //beans section
+  const beans = document.createElement('h1');
+  beans.textContent = 'Beans';
+  menuDiv.appendChild(beans);
+
+  const img3 = document.createElement('img');
+  img3.src = bean;
+  menuDiv.appendChild(img3);
+  img3.classList.add('img3');
+  
 
   return menuDiv
 };
